deee976ae7852d019781a8b8a88741713b1a519d
Ali Can YILDIZ <anotherglitchinthematrix@gmail.com>
2019-08-20 10:08:13 +0300

    Grid tweak

--- src/Admirable/Scenes/Menu/Layers/GridLayer.ts
+++ src/Admirable/Scenes/Menu/Layers/GridLayer.ts
@@ -5,29 +5,24 @@ import { transcode } from 'buffer';
 
 export class GridLayer extends Layer {
 
-  private walk: number = 0;
+  private angle: number = 0;
 
   private gridSprite: ISprite;
   private gridGraphics: Graphics;
   private gridOptions: any = {
     animate: true,
     frequency: 50,
-    gridGap: 25,
-    gridX: 0,
-    gridY: 0,
     shiftY: -150,
   };
 
-  public constructor(animate: boolean = true, gap: number = 25) {
+  public constructor(animate: boolean = true, frequency: number = 50, shiftY: number = -150) {
     // parent call
     super();
 
     // Grid Options
     this.gridOptions.animate = animate;
-    this.gridOptions.gridGap = gap;
-    this.gridOptions.gridX = Math.ceil(this.screen.dimensions.width / this.gridOptions.gridGap);
-    this.gridOptions.gridY = Math.ceil(this.screen.dimensions.height / this.gridOptions.gridGap);
-
+    this.gridOptions.frequency = frequency;
+    this.gridOptions.shiftY = shiftY;
     // Create grid sprite
     this.gridGraphics = this.createGraphics(this.screen.dimensions.width, this.screen.dimensions.height);
     this.gridSprite = {
@@ -42,26 +37,26 @@ export class GridLayer extends Layer {
 
   public update(): void {
     this.gridGraphics.background(0, 25);
-    this.gridGraphics.stroke(0, 255, 255, 25);
+    this.gridGraphics.stroke(0, 255, 255, 50);
 
-    this.perspectiveGrid(this.walk);
+    this.perspectiveGrid(this.angle);
     // can't use this.gridGraphics.deltaTime here.
-    this.walk = this.walk + (this.p.deltaTime / 100000);
+    this.angle = this.angle + (this.p.deltaTime / 100000);
   }
 
   /**
    * Creates horizontal lines with given angle.
-   * @param a angle at a specific time.
+   * @param angle angle at a specific time.
    */
-  private perspectiveGrid(a: number) {
+  private perspectiveGrid(angle: number) {
     const width = this.screen.dimensions.width;
     const da = this.gridGraphics.TWO_PI / width;
 
-    for (let i = 0; i < width / 2; i++) {
+    for (let i = 0; i < width / 4; i++) {
       const x1 = (i * 4),
             y1 = 0,
             x2 = x1,
-            y2 = this.gridOptions.shiftY + this.gridGraphics.tan(a) * this.gridOptions.frequency;
+            y2 = this.gridOptions.shiftY + this.gridGraphics.tan(angle) * this.gridOptions.frequency;
 
       // Debug graphic;
       // this.gridGraphics.line(x1, y1, x2, y2);
@@ -69,7 +64,7 @@ export class GridLayer extends Layer {
       // Perspectve lines
       this.gridGraphics.line(0, y2, width, y2);
 
-      a = (a + da) % this.gridGraphics.HALF_PI;
+      angle = (angle + da) % this.gridGraphics.HALF_PI;
     }
   }
 
