e91aabb87c9cea5684afff1924f4ebea639e9219
Ali Can YILDIZ <anotherglitchinthematrix@gmail.com>
2019-09-02 17:49:20 +0300

    Grid Snap Test

--- src/Admirable/Scenes/SetupScene/setup.scene.ts
+++ src/Admirable/Scenes/SetupScene/setup.scene.ts
@@ -14,6 +14,77 @@ export class SetupScene extends Phaser.Scene {
   public create(): void {
     this.grid = this.add.graphics();
     this.drawGrid();
+
+    // Snap Test
+    const sip = this.add.image(40, 40, 'test').setOrigin(0).setInteractive();
+
+    sip.setDisplaySize(40, 40);
+
+    this.input.setDraggable(sip);
+
+    sip.on('drag', (p: any, x: any, y: any) => {
+      // const dX = Phaser.Math.Snap.To(x, 40, 1);
+      // const dY = Phaser.Math.Snap.To(y, 40, 1);
+      sip.x = x;
+      sip.y = y;
+    });
+
+    sip.on('dragend', (p: any) => {
+      const dX = Phaser.Math.Snap.To(p.upX, 40);
+      const dY = Phaser.Math.Snap.To(p.upY, 40);
+      sip.x = dX;
+      sip.y = dY;
+    });
+
+    const ShipContainer = this.add.container(0, 0);
+    const s1  = this.add.image(0, 0, 'test').setOrigin(0).setDisplaySize(40, 40);
+    const s2  = this.add.image(0, 40, 'test').setOrigin(0).setDisplaySize(40, 40);
+    const s3  = this.add.image(0, 80, 'test').setOrigin(0).setDisplaySize(40, 40);
+    const bound = new Phaser.GameObjects.Rectangle(this, -40, -40, 120, 200, 0x00A8E8, 0.1).setOrigin(0);
+    ShipContainer.add([s1, s2, s3]);
+
+    ShipContainer.setInteractive(ShipContainer.getBounds(), Phaser.Geom.Rectangle.Contains);
+    this.input.setDraggable(ShipContainer);
+    const bounds = ShipContainer.getBounds();
+    // console.log(ShipContainer.getBounds());
+    // ShipContainer.angle = 90;
+    // container pozisyonu setinteractiveden sonra verilmeli, aksi halde, hitarea ile container
+    // tutarsız davranıyor.
+
+    ShipContainer.addAt(bound, 0);
+
+    ShipContainer.on('dragstart', () => {
+      bound.setFillStyle(0x00A8E8, 0.3);
+    });
+
+    ShipContainer.on('dragend', () => {
+      bound.setFillStyle(0x00A8E8, 0.1);
+    });
+
+    ShipContainer.setPosition(560, 160);
+    const canvasHeight = this.sys.canvas.height,
+          canvasWidth = this.sys.canvas.width;
+    ShipContainer.on('drag', (p: any, x: any, y: any) => {
+      // console.log(ShipContainer.angle);
+      let dX, dY;
+
+      if (x < (canvasWidth - 40) / 2) {
+        dX = Phaser.Math.Snap.To(x, 40, 1);
+        dY = Phaser.Math.Snap.To(y, 40, 1);
+      } else {
+        dX = x;
+        dY = y;
+      }
+
+      dX = Math.max(dX, 1);
+      dX = Math.min(dX, canvasWidth - bounds.width);
+      dY = Math.max(dY, 1);
+      dY = Math.min(dY, canvasHeight - bounds.height);
+
+      ShipContainer.x = dX;
+      ShipContainer.y = dY;
+    });
+
   }
 
   private drawGrid() {
