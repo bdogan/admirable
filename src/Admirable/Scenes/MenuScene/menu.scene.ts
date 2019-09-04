6fdd8e0697549b0854fa732e9f49516efda2f683
Ali Can YILDIZ <anotherglitchinthematrix@gmail.com>
2019-09-04 09:14:44 +0300

    MenuScene clean-up

--- src/Admirable/Scenes/MenuScene/menu.scene.ts
+++ src/Admirable/Scenes/MenuScene/menu.scene.ts
@@ -10,18 +10,6 @@ export class MenuScene extends Phaser.Scene {
 
   private grid!: Phaser.GameObjects.Graphics;
 
-  private imgBattleShip?: any;
-  private battleShipAngle: number = 0;
-  private imgLogo?: any;
-  private easeRate: number = 0.10;
-  private gridOptions: any = {
-    animate: true,
-    frequency: 50,
-    shiftY: -150,
-  };
-  private gridGraphics!: any;
-  private gridAngle: number = 0;
-
   public preload() {
     this.load.image('logo', logoImg);
     this.load.image('battleship', battleshipImg);
@@ -32,16 +20,18 @@ export class MenuScene extends Phaser.Scene {
   }
 
   public create(): void {
+
     this.grid = this.add.graphics();
-    this.imgBattleShip = this.add.image(0, this.sys.game.canvas.height + 20, 'battleship');
-    this.imgBattleShip.setOrigin(0, 1);
 
-    this.imgLogo = this.add.image(0, 0, 'logo');
-    this.imgLogo.setPosition(this.sys.canvas.width / 2 , -this.imgLogo.height);
-    this.gridGraphics = this.add.graphics();
+    const imgBattleShip = this.add.image(0, this.sys.game.canvas.height + 20, 'battleship');
+    imgBattleShip.setOrigin(0, 1);
 
+    const imgLogo = this.add.image(0, 0, 'logo');
+    imgLogo.setPosition(this.sys.canvas.width / 2, -imgLogo.height);
+
+    // Continuous animation for batlleship.
     this.tweens.add({
-      targets: this.imgBattleShip,
+      targets: [imgBattleShip],
       y: this.sys.game.canvas.height + 50,
       ease: 'Sine.easeInOut',
       duration: 1000,
@@ -49,8 +39,9 @@ export class MenuScene extends Phaser.Scene {
       loop: -1
     });
 
+    // Animation for logo entrance.
     this.tweens.add({
-      targets: [this.imgLogo],
+      targets: [imgLogo],
       y: {
         getStart: () => 0,
         getEnd: () => this.sys.canvas.height / 4
@@ -63,7 +54,8 @@ export class MenuScene extends Phaser.Scene {
       duration: 2000
     });
 
-    const startButton =  new Button(this, 'START', 960 / 2, 480 / 2);
+    // Add start button to the scene.
+    const startButton = new Button(this, 'START', 960 / 2, 480 / 2);
 
     this.add.existing(startButton);
 
@@ -77,11 +69,12 @@ export class MenuScene extends Phaser.Scene {
     this.drawHorizon(time);
   }
 
+  // Horizontal lines.
   private drawHorizon(a: number) {
 
     let dt = a / 100000;
-    const width   = this.sys.canvas.width,
-          height  = this.sys.canvas.height;
+    const width  = this.sys.canvas.width,
+          height = this.sys.canvas.height;
 
     const TWO_PI = Phaser.Math.PI2;
     const dA = TWO_PI / width;
@@ -89,15 +82,17 @@ export class MenuScene extends Phaser.Scene {
     this.grid.clear();
 
     this.grid.lineStyle(1, 0x00A8E8, 0.5);
-    for (let line = 0, lines = height ; line < lines; line++) {
+    for (let line = 0, lines = height; line < lines; line++) {
 
       const x1 = (line * 4), y1 = 0, x2 = x1, y2 = -200 + Math.tan(dt) * 50;
+
       this.grid.lineBetween(0, y2, width, y2);
+
       dt = (dt + dA);
+
     }
 
     this.grid.strokePath();
-
-}
+  }
 
 }
