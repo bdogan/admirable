537fb2e05e2a4ca5602e774c157361d8c13beddb
hkrabs <hkrabs@yandex.com>
2019-09-09 10:56:24 +0300

    Improvements

--- src/Admirable/admirable.ts
+++ src/Admirable/admirable.ts
@@ -3,11 +3,15 @@ import { AdmirableGame } from './admirable.game';
 import { AdmirableConfig } from './admirable.config';
 import { DemoScene, MenuScene, GameScene, SetupScene } from './Scenes';
 
-export const Game = new AdmirableGame(AdmirableConfig({
-  scene: [
-    SetupScene,
-    MenuScene,
-    DemoScene,
-    GameScene,
-  ],
-}));
+export const Game = document.onreadystatechange = () => {
+  if (document.readyState === 'complete') {
+    return new AdmirableGame(AdmirableConfig({
+      scene: [
+        MenuScene,
+        SetupScene,
+        DemoScene,
+        GameScene,
+      ],
+    }));
+  }
+};
